6d50a6f722a4189396199d244cbaca1361eb9a11
kanekou <e165726@ie.u-ryukyu.ac.jp>
2023-09-19 20:09:03 +0900

    背景色とfontの変更

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 const MoreStories = ({ posts, title = "パンうめえ🥯" }: Props) => {
   return (
-    <section className="max-w-4xl align-center mx-auto">
-      <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
+    <section className="max-w-4xl mx-auto">
+      <h2 className="mb-8 text-5xl md:text-7xl font-kosugiMaru tracking-tighter leading-tight content-center">
         {title}
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 pb-32">
