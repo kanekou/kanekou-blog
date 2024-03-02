5848760eaeddef5be4a113b821be20849f3d60cb
kanekou <e165726@ie.u-ryukyu.ac.jp>
2024-03-02 12:50:34 +0900

    日付の位置を変更

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -8,16 +8,15 @@ type Props = {
   title: string;
   coverImage: string;
   date: string;
-  author: Author;
   tags: string[];
 };
 
-const PostHeader = ({ title, coverImage, date, author, tags }: Props) => {
+const PostHeader = ({ title, coverImage, date, tags }: Props) => {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
+      <div className="mb-6 text-right">
+        <DateFormatter dateString={date} />
       </div>
       {tags.map((tag, index) => (
         <span
@@ -30,14 +29,7 @@ const PostHeader = ({ title, coverImage, date, author, tags }: Props) => {
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
       </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
-        </div>
-        <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
-        </div>
-      </div>
+      <div className="max-w-2xl mx-auto"></div>
     </>
   );
 };
