edeae034058880bb8a189290eb3e3c2c001f9b67
kanekou <e165726@ie.u-ryukyu.ac.jp>
2024-07-15 22:36:37 +0900

    faviconとapple-touch-iconの設定

--- components/meta.tsx
+++ components/meta.tsx
@@ -19,7 +19,7 @@ const Meta = ({
 }: Props) => {
   return (
     <Head>
-      {/* <link
+      <link
         rel="apple-touch-icon"
         sizes="180x180"
         href="/favicon/apple-touch-icon.png"
@@ -37,7 +37,7 @@ const Meta = ({
         href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
+      {/* <link
         rel="mask-icon"
         href="/favicon/safari-pinned-tab.svg"
         color="#000000"
