9a38fb6ae056d1bd6d8a8479d35edf1640a50707
kanekou <e165726@ie.u-ryukyu.ac.jp>
2024-07-16 22:01:05 +0900

    header情報をfaviconが読み込まれるよう変更

--- components/meta.tsx
+++ components/meta.tsx
@@ -37,15 +37,11 @@ const Meta = ({
         href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      {/* <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
+      <link rel="mask-icon" href="/favicon/favicon-32x32.png" color="#000000" />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" /> */}
+      <meta name="theme-color" content="#000" />
       {/* <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
       <title>{title}</title>
       <meta name="description" content={description} />
