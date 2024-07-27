e68ad91f510d8957127f7530ee8c519da3aa40a2
kanekou <e165726@ie.u-ryukyu.ac.jp>
2024-07-27 17:37:16 +0900

    ページの表題のスタイルを統一

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -4,19 +4,16 @@ import type Post from "../interfaces/post";
 type Props = {
   posts: Post[];
   title: string;
-  fromHome?: boolean;
 };
 
-const getTitleClass = (fromHome: boolean) => {
-  return fromHome
-    ? "mb-8 text-5xl md:text-7xl font-kosugiMaru tracking-tighter leading-tight content-center"
-    : "mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight";
+export const getTitleClass = () => {
+  return "mb-8 text-5xl md:text-7xl font-kosugiMaru tracking-tighter leading-tight content-center";
 };
 
-const MoreStories = ({ posts, title, fromHome = true }: Props) => {
+const MoreStories = ({ posts, title }: Props) => {
   return (
     <section className="max-w-7xl mx-auto">
-      <h2 className={getTitleClass(fromHome)}>{title}</h2>
+      <h2 className={getTitleClass()}>{title}</h2>
       <div className="grid sm:grid-cols-2 gap-4 mb-12 w-full">
         {posts.map((post) => (
           <PostPreview
