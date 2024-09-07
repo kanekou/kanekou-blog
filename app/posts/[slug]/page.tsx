fa4280d9c9c4b70ec846706b770c2d9d19c5e2d3
kanekou <e165726@ie.u-ryukyu.ac.jp>
2024-09-07 17:21:27 +0900

    サイト全体と記事ページのOGPを設定

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -5,16 +5,38 @@ import Layout from "../../../components/layout";
 import { getPostBySlug, getAllPosts } from "../../../lib/api";
 import Head from "next/head";
 import type PostType from "../../../interfaces/post";
+import type { Metadata, ResolvingMetadata } from "next";
+import { SITE_URL } from "../../../lib/constants";
 
-export default async function Post({ params }: any) {
+type Props = {
+  params: { slug: string };
+};
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
   const post = await getPost(params.slug);
   const ogImage = `${process.env.NEXT_PUBLIC_IMAGE_HOST}${post.ogImage.url}`;
+  const pathName = `${SITE_URL}/posts/${post.slug}`;
+  const previousOG = (await parent).openGraph;
+  return {
+    openGraph: {
+      ...previousOG,
+      url: pathName,
+      images: [ogImage],
+      type: "article",
+    },
+  };
+}
+
+export default async function Post({ params }: Props) {
+  const post = await getPost(params.slug);
   if (!post?.slug) {
     // TODO: 後に404pageを追加する
     return "not found page";
   }
   return (
-    <Layout ogImage={ogImage}>
+    <Layout>
       <Container>
         <>
           <Head>
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,35 +1,19 @@
 import Alert from "./alert";
-import Meta from "./meta";
 import Header from "./header";
-import { SITE_OG_IMAGE } from "../lib/constants";
 
 type Props = {
   preview?: boolean;
-  ogImage?: string;
   children: React.ReactNode;
 };
 
-const Layout = ({
-  preview = false,
-  ogImage = SITE_OG_IMAGE,
-  children,
-}: Props) => {
+const Layout = ({ preview = false, children }: Props) => {
   return (
-    <>
-      <Meta
-        title="パンうめえ"
-        description="kanekouの技術ブログです"
-        url="https://kanekou-blog.web.app"
-        type="blog"
-        ogImage={ogImage}
-      />
-      <div className="min-h-screen">
-        <Header />
-        {preview && <Alert />}
+    <div className="min-h-screen">
+      <Header />
+      {preview && <Alert />}
 
-        <main>{children}</main>
-      </div>
-    </>
+      <main>{children}</main>
+    </div>
   );
 };
 
