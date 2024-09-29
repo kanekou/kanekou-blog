8ec2155465b83d64a64b6423481fdfdde041a967
kanekou <e165726@ie.u-ryukyu.ac.jp>
2024-09-29 16:29:25 +0900

    footerのレイアウト変更

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="bg-gray-200 p-4 text-center text-surface">
+    <footer className="w-full bg-gray-200 p-4 text-center text-surface">
       ©︎ 2024 kanekou
     </footer>
   );
