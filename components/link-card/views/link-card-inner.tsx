05c936cfd7c44fd97a4b205a9867f91280a193d3
kanekou <e165726@ie.u-ryukyu.ac.jp>
2024-11-23 12:58:57 +0900

    link-cardのレイアウト修正

--- components/link-card/views/link-card-inner.tsx
+++ components/link-card/views/link-card-inner.tsx
@@ -1,3 +1,4 @@
+import getFavicon from "../../../lib/getFavicon";
 import getMetaData from "../../../lib/getMetaData";
 import LinkCardError from "./link-card-error";
 
@@ -12,10 +13,11 @@ const LinkCardInner = async ({
   if (!meta) {
     return <LinkCardError href={href} />;
   }
+  const url = new URL(href);
   return (
     <a
       href={href}
-      className="flex items-center bg-white border border-gray-200 rounded-lg shadow no-underline"
+      className="flex bg-white border border-gray-200 rounded-lg shadow no-underline"
       target="_blank"
       rel="noreferrer"
     >
@@ -23,8 +25,16 @@ const LinkCardInner = async ({
         <div className="text-lg font-bold tracking-tight text-gray-900 line-clamp-2">
           {meta.title}
         </div>
-        <div className="truncate mb-3 font-normal text-gray-700">
+        <div className="truncate font-normal text-gray-700">
           {meta.description}
+        </div>
+        <div className="text-[0.78em] flex -mt-2 items-center overflow-hidden whitespace-nowrap">
+          <img
+            className="w-4 h-4"
+            src={getFavicon(url.hostname)}
+            alt={meta.title}
+          />
+          <span className="text-gray-700 ml-1">{url.hostname}</span>
         </div>
       </div>
       {meta.image && (
